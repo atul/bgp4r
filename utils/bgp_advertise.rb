43d5d791f751423841ccee2933965446d1c48217
Atul Patel <atul@cumulusnetworks.com>
2014-05-08 00:17:24 -0700

    Script to advertise bgp prefixes.

--- utils/bgp_advertise.rb
+++ utils/bgp_advertise.rb
@@ -9,42 +9,49 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: bgp_ipv4_alpm.rb [options]"
 
-  opts.on('-a', '--afi', 'Address Family') { |v| options[:afi] = v }
-#  opts.on('-s', '--safi', 'Source host') { |v| options[:safi] = v }
-  opts.on('-n', '--number', 'number of prefixes') { |v| options[:@times] = v }
+  opts.on('-r', '--remote_addr IPADDR', 'Remote Address') { |v| options[:remote_add] = v }
+  opts.on('-l', '--local_address IPADDR', 'Local address') { |v| options[:local_add] = v }
+  opts.on('-a', '--local_as ASN', 'Local ASN') { |v| options[:local_as] = v }
+  opts.on('-4', '--v4prefixes INTEGER', 'number of v4 prefixes') { |v| options[:times4] = v }
+  opts.on('-6', '--v6prefixes INTEGER', 'number of v4 prefixes') { |v| options[:times6] = v }
+  opts.on('-h', '--nh_address6 NHADDR', 'IPv6 NH address') { |v| options[:nh6] = v }
+
 
 end.parse!
 
-#dest_options = YAML.load_file('destination_config.yaml')
-puts options[:@times]
-
-v4prefixes = 10
-v6prefixes = 10
+puts options[:@local_add]
+pp options
+@remote_add = options[:remote_add]
+@local_add = options[:local_add]
+@local_as = options[:local_as]
+@times4 = options[:times4]
+@times6 = options[:times6]
+@nh6 = options[:nh6]
 
 neighbor = Neighbor.new \
   :version => 4,
-  :my_as=> 600,
-  :remote_addr => '210.3.2.3',
-  :local_addr => '210.3.2.6',
+  :my_as=> @local_as,
+  :remote_addr => @remote_add,
+  :local_addr => @local_add,
   :id=> '3.3.3.3',
   :holdtime=> 180
 
- neighbor.capability :as4_byte
-# neighbor.capability :as4_byte
-# neighbor.capability :as4_byte
+neighbor.capability_mbgp_ipv4_unicast
+neighbor.capability_mbgp_ipv6_unicast
+neighbor.capability :as4_byte
 
 pav4 = Path_attribute.new(
-  Origin.new("0"),
-  Next_hop.new("210.3.2.6"),
-  Local_pref.new("200"),
+  Origin.new(0),
+  Next_hop.new(@local_add),
+  Local_pref.new(200),
   Communities.new(*("100:1 200:1 300:1 2938:22 324:3432 3344:343 4466:6436 5445:3454 3545:5677 5754:5754".split.map { |com| com.to_i})),
   As_path.new(*("790 90 80 2334 544 56 67 889 3111 777 8 879 0900 88 7654 3211 113 43434 666 343 4534 667 7688".split.map { |as| as.to_i}))
 )
 
 pav6 = Path_attribute.new(
-    Origin.new("0"),
-    Next_hop.new("::6"),
-    Local_pref.new("200"),
+    Origin.new(0),
+    Next_hop.new(@nh6),
+    Local_pref.new(200),
     Communities.new(*("100:1 200:1 300:1 2938:22 324:3432 3344:343 4466:6436 5445:3454 3545:5677 5754:5754".split.map { |com| com.to_i})),
     As_path.new(*("790 90 80 2334 544 56 67 889 3111 777 8 879 0900 88 7654 3211 113 43434 666 343 4534 667 7688".split.map { |as| as.to_i}))
 )
@@ -54,33 +61,35 @@ pav6 = Path_attribute.new(
 
 neighbor.start
 
-
-@nlri4 = IPAddr.new "20.0.0.0/28"
-sender1 = File.open("sender1", 'w')
-nlris = Nlri.new
-(1..@times.to_i).each do |n|
-  sender1.write('mz -c 3 -B %s -t udp dp=999 -A 210.4.2.5 &' % (IPAddr.new(@nlri4 ^ n) + 1))
-  sender1.write("sleep 2\n") if (n % 500) == 0
-  nlris << (@nlri4 ^ n)
-  next unless (n % @pack) == 0 or (n == @times)
-  neighbor.send_message Update.new(pav4, nlris)
+begin
+  @nlri4 = IPAddr.new "20.0.0.0/28"
+  sender1 = File.open("sender1", 'w')
   nlris = Nlri.new
-end
+    (1..@times.to_i).each do |n|
+     sender1.write('mz -c 1 -B %s -t udp dp=999 -A #{@local_add} &' % (IPAddr.new(@nlri4 ^ n) + 1))
+     sender1.write("sleep 2\n") if (n % 500) == 0
+     nlris << (@nlri4 ^ n)
+     next unless (n % @pack) == 0 or (n == @times4)
+     neighbor.send_message Update.new(pav4, nlris)
+     nlris = Nlri.new
+  end
 
-sender1.close()
+  sender1.close()
+end if @times4
 
-=begin
+begin
 
-@nlri6 = IPAddr.new "5000:9999:8888:1::0/64"
-(1..@times.to_i).each do |n|
-  nlris << (@nlri6 ^ n)
-  next unless (n % @pack) == 0 or (n == @times)
-  neighbor.send_message Update.new(pav4, nlris)
-  nlris = Nlri.new
-end
-=end
+  @nlri6 = IPAddr.new "5000:9999:8888:1::0/64"
+   (1..@times.to_i).each do |n|
+   nlris << (@nlri6 ^ n)
+   next unless (n % @pack) == 0 or (n == @times)
+   neighbor.send_message Update.new(pav4, nlris)
+   nlris = Nlri.new
+  end
 
-sleep(1800)
+end if @times6
+
+#sleep(1800)
 
 =begin
 require 'bgp4r'
