2d9010e925200121b5cf9aec1e42dcf8a06e962b
Atul Patel <atul@cumulusnetworks.com>
2014-05-07 17:18:14 -0700

    IPv6 prefixes for test.

--- utils/bgp_ipv6_alpm.rb
+++ utils/bgp_ipv6_alpm.rb
@@ -8,14 +8,16 @@ Log.level=Logger::INFO
 neighbor = Neighbor.new \
 :version=> 4,
 :my_as=> 100,
-:remote_addr => '169.253.253.102',
+:remote_addr => '210.3.2.3',
+:local_address => '210.3.2.6',
 :id=> '20.20.20.20',
 :holdtime=> 180
 
 neighbor.capability_mbgp_ipv4_unicast
 neighbor.capability_mbgp_ipv6_unicast
 
-nexthop='fe80::20c:29ff:fcab:13b'
+#nexthop='fe80::20c:29ff:fcab:13b'
+nexthop='2210:210:3:2::6'
 
 pa = Path_attribute.new(
     Origin.new(0),
@@ -25,10 +27,10 @@ pa = Path_attribute.new(
 
 # 22 routes, 5 routes per update
 subnet = Fiber.new do
-  ipaddr = IPAddr.new "2014:13:11::1/65"
+  ipaddr = IPAddr.new "2014:13:11::1/64"
   pack=5
   prefixes=[]
-  22.times do |i|
+  16000.times do |i|
     prefixes << (ipaddr ^ i)
     next unless (i%pack)==0
     Fiber.yield prefixes
