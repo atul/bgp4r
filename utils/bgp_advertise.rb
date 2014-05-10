2f5c861a32378506c667e8837c2b9b1515187996
Atul Patel <atul@cumulusnetworks.com>
2014-05-10 10:32:41 -0700

    Script to advertise bgp prefixes (IPv4 and IPv6).

--- utils/bgp_advertise.rb
+++ utils/bgp_advertise.rb
@@ -38,7 +38,7 @@ neighbor = Neighbor.new \
 
 neighbor.capability_mbgp_ipv4_unicast
 neighbor.capability_mbgp_ipv6_unicast
-#neighbor.capability :as4_byte
+neighbor.capability :as4_byte
 
 pa4 = Path_attribute.new(
   Origin.new(0),
@@ -52,7 +52,6 @@ pp pa4
 
 pa6 = Path_attribute.new(
     Origin.new(0),
-    #Local_pref.new(200),
     Communities.new(*("100:1 200:1 300:1 2938:22 324:3432 3344:343 4466:6436 5445:3454 3545:5677 5754:5754".split.map { |com| com.to_i})),
     As_path.new(*("790 90 80 2334 544 56 67 889 3111 777 8 879 0900 88 7654 3211 113 43434 666 343 4534 667 7688".split.map { |as| as.to_i}))
 )
@@ -94,76 +93,5 @@ end if @times6
 
 
 sleep(1800)
-
-=begin
-require 'bgp4r'
-require 'yaml'
-include BGP
-v4prefixes = 10
-v6prefixes = 10
-
-require 'pp'
-@times = 10
-@pack = 127
-
-@nlri4 = IPAddr.new "20.0.0.0/28"
-(1..@times.to_i).each do |n|
-pp 'mz -c 3 -B %s -t udp dp=999 -A 210.4.2.5 &' % (IPAddr.new(@nlri4 ^ n) + 1).split(/\//)[0]
-pp 'sleep 2' if (n % 3) == 0
-next unless (n % 127) == 0 or (n== @times)
-end
-
-@nlri6 = IPAddr.new "5000:9999:8888:1::0/64"
-(1..@times.to_i).each do |n|
-#  nlris << (@nlri6 ^ n)
-
-  next unless (n % @pack) == 0 or (n == @times)
-#  neighbor.send_message Update.new(pav4, nlris)
-#  nlris = Nlri.new
-end
-
--------------------------------------
-require 'bgp4r'
-
-include BGP
-
-Log.create
-Log.level=Logger::DEBUG
-
-neighbor = Neighbor.new \
-    :version=> 4,
-    :my_as=> 100,
-    :remote_addr => '169.253.253.128',
-    :id=> '20.20.20.20',
-    :holdtime=> 180
-
-  neighbor.capability_mbgp_ipv4_unicast
-  neighbor.capability_mbgp_ipv6_unicast
-
- nexthop='fe80::20c:29ff:fcab:13b'
-
- pa = Path_attribute.new(
-   Origin.new(0),
-   Multi_exit_disc.new(0),
-   As_path.new(),
-   Mp_reach.new(:afi=>2, :safi=>1, :nexthop=> nexthop,
-                :nlris=> ['2014:50:4::/64', '2014:51:4::/64', '2014:52:4::/64'])
- )
-
- neighbor.start
-
- Log.level=Logger::INFO
-
- neighbor.send_message Update.new(pa)
-
- sleep (300)
-
-----------------
-require 'bgp4r'
-include BGP
-require 'ipaddress'
-
-
-=end
 
 
